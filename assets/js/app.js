231c03aad990ce74633bc050cbba772f07e2dd32
parth sharma <parthsharma@gmail.com>
2024-05-02 11:59:49 +0530

    Navbar update

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,3 +38,26 @@ menuIcon.addEventListener("click", () => {
     document.querySelector("nav").classList.toggle("navbg");
     document.querySelector("body").classList.toggle("overflow-hidden");
 })
+
+// countdown
+
+const targetDate = new Date();
+targetDate.setDate(targetDate.getDate() + 19);
+targetDate.setHours(targetDate.getHours() + 10);
+targetDate.setMinutes(targetDate.getMinutes() + 2);
+targetDate.setSeconds(targetDate.getSeconds() + 5);
+function updateCountdown() {
+    const now = new Date();
+    const timeLeft = targetDate - now;
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    const countdownElement = document.getElementById('countdown');
+    countdownElement.textContent = `${days}d:${hours}hr:${minutes}min:${seconds}sec`;
+    if (timeLeft < 0) {
+        clearInterval(timerInterval);
+        countdownElement.textContent = "Countdown is over!";
+    }
+}
+const timerInterval = setInterval(updateCountdown, 1000);
